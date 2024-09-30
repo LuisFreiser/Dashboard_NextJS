3bf575a9d3445f327871fc7ec84ec2570b87befb
Luis Suarez <luisfreiser@gmail.com>
2024-09-30 01:06:23 -0500

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import './ui/global.css';
+import {montserrat} from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {/* <h1 className="text-3xl font-bold text-blue-500">CURSO DE NEXTJS 2024</h1> */}
+        {children}
+        </body>
     </html>
   );
 }
